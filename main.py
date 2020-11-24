d3a085f3be87af299dceb1728356aec0703bec3b
Pham Duc Hoang <36380799+phamduchoangeee@users.noreply.github.com>
2020-11-24 20:35:29 +0900

    stage 1

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import sys
 import CONST
 
 np.set_printoptions(threshold=sys.maxsize)
-from NoPlaceDetect import GetPlateNumber
+
 
 def main():
     # load image
-    img = _utils.read_img("2.jpg")
+    img = _utils.read_img("9.jpg")
     img_result = img.copy()
     # get salient objects from image
     salientObjects, cars_no = processing.get_cars(img)
@@ -20,21 +20,18 @@ def main():
         # get the bounding box coordinates
         (startX, startY, endX, endY) = salientObjects[i].flatten()
         # 60% of the cropped area, for important information because plate_number always lay below car
-        startY = int((startY + endY)*(1-CONST.TARGETED_OBJECT))
+        startY = int((startY + endY) * (1 - CONST.TARGETED_OBJECT))
         car_box = img[startY:endY, startX:endX]
-        car_box = GetPlateNumber.get_number_plate(car_box)
-        if car_box is not None:
-            _utils.show_img("car_box_number", car_box)
-        # number_plate = processing.get_number_plate(car_box)
-        # if number_plate is not None:
-        #     print("FOUND! Number_plate")
-        #     # debugging
-        #     car_box_debugging = cv2.drawContours(car_box.copy(), number_plate, -1, (0, 255, 0), 3)
-        #     _utils.show_img("car_box", car_box_debugging)
-        #     # end debugging
-        #     cv2.drawContours(img_result, number_plate, -1, (0, 255, 0), thickness=3, offset=(startX, startY))
-        # else:
-        #     print("NOT FOUND! Number_plate")
+        number_plate = processing.get_number_plate(car_box)
+        if number_plate is not None:
+            print("FOUND! Number_plate")
+            # debugging
+            # car_box_debugging = cv2.drawContours(car_box.copy(), number_plate, -1, (0, 255, 0), 3)
+            # _utils.show_img("car_box", car_box_debugging)
+            # end debugging
+            cv2.drawContours(img_result, number_plate, -1, (0, 255, 0), thickness=3, offset=(startX, startY))
+        else:
+            print("NOT FOUND! Number_plate")
     _utils.show_img("result", img_result)
 
 
--- processing.py
+++ processing.py
@@ -35,21 +35,23 @@ def get_number_plate(car_bbox):
     :param car_bbox: input proposed cars (bounding box of the car)
     """
     print("Start getting car number plate...")
+    _utils.show_img("car_bbox", car_bbox)
     # gray scale
     gray = cv2.cvtColor(car_bbox, cv2.COLOR_BGR2GRAY)  # convert to grey scale
-    gray = cv2.bilateralFilter(gray, 11, 18, 18)
+    gray = cv2.bilateralFilter(gray, 5, 35, 35)
     # get edged
     edged = cv2.Canny(gray, 30, 200)
     _utils.show_img("edged", edged)
+
     # get contours
     contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(contours)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:CONST.MAX_TAKEN_CONTOUR]
-    _utils.show_img("car_bbox", car_bbox)
     # debugging draw all contours
     car_box_debugging = cv2.drawContours(car_bbox.copy(), contours, -1, CONST.CONTOUR_COLOR, CONST.CONTOUR_SIZE)
     _utils.show_img("car_box_debugging", car_box_debugging)
     rectangle_detected = None
+    max_chars_len = 0
     for c in contours:
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, CONST.APRROX_POLY_DP * peri, True)
@@ -57,28 +59,25 @@ def get_number_plate(car_bbox):
             rectangle_detected = approx
             break
 
-    # text = get_number_plate_text([rectangle_detected], car_bbox, gray)
-    # print(text)
-
     if rectangle_detected is None:
         return None
     else:
         return [rectangle_detected]
 
 
-def get_number_plate_text(rectangle, origin_img, origin_gray):
+def get_chars_length(rectangle, origin_img, origin_gray):
     # Masking the part other than the number plate
     mask = np.zeros(origin_gray.shape, np.uint8)
-    cv2.drawContours(mask, rectangle, 0, 255, -1, )
-    number_plate_alone = cv2.bitwise_and(origin_img, origin_img, mask=mask)
-    _utils.show_img("number_plate_alone", number_plate_alone)
-    # Now crop
-    (x, y) = np.where(mask == 255)
-    (topx, topy) = (np.min(x), np.min(y))
-    (bottomx, bottomy) = (np.max(x), np.max(y))
-    Cropped = origin_gray[topx:bottomx + 1, topy:bottomy + 1]
+    try:
+        cv2.drawContours(mask, rectangle, 0, 255, -1, )
+    except cv2.error:
+        return 0
+
+    Cropped = cv2.bitwise_and(origin_img, origin_img, mask=mask)
+    _utils.show_img("Cropped", Cropped)
     # Read the number plate
     text = pytesseract.image_to_string(Cropped, config='--psm 11')
-    print("Detected Number is:", text)
-    return text
-
+    if text is not None:
+        print(text)
+        return len(text)
+    return 0
